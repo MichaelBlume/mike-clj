ea3df99e83c445cbaf6064eeb4c4c992c9e07fe6
Mike Blume <michael.blume@climate.com>
2014-09-17 16:56:03 -0700

    add add-dep macro

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject mike-clj "1.0.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.cemerick/pomegranate "0.3.0"]])
--- src/mike.clj
+++ src/mike.clj
@@ -1,5 +1,6 @@
 (ns mike
-  (:require [clojure.test :refer [run-tests]]))
+  (:require [clojure.test :refer [run-tests]]
+            [cemerick.pomegranate :refer [add-dependencies]]))
 
 ; Stuff I always want
 
@@ -18,3 +19,12 @@
 
 (defn run-all-tests []
   (apply run-tests (all-test-ns)))
+
+(defn add-dep* [d]
+  (add-dependencies
+    :coordinates [d]
+    :repositories (merge cemerick.pomegranate.aether/maven-central
+                         {"clojars" "http://clojars.org/repo"})))
+
+(defmacro add-dep [d]
+  `(add-dep* (quote ~d)))
