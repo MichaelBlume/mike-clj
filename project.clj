66ee5b84ca039bc8959b60a4ebb25b2ca2772638
Mike Blume <michael.blume@climate.com>
2015-02-03 12:52:57 -0800

    upgrade tools.namespace

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.namespace "0.2.7"]
+                 [org.clojure/tools.namespace "0.2.9"]
                  [com.cemerick/pomegranate "0.3.0"]])
