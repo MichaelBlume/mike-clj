bcfa98c8a7701b4b191bcd54db48338965298e17
Michael Blume <michael.blume@climate.com>
2015-08-21 15:28:11 -0700

    begin 1.0.1 development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject editor-fns "1.0.0"
+(defproject editor-fns "1.0.1-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
